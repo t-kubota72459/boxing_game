ec98ff041804ddb3eb59e0aaa28b3de1d2672c94
t-kubota72459 <t-kubota72459@vt-school.hiroshima.jp>
2024-10-24 00:17:35 +0100

    modified

--- M5AtomS3/main/main.ino
+++ M5AtomS3/main/main.ino
@@ -4,21 +4,35 @@
 #define MY_PEER "C2:FD:63:8F:CC:42"
 
 microbit mymb = microbit();
+QueueHandle_t queue;
+
+void task1(void* pvParameters) {
+    uint16_t b;
+    while (1) {
+      b = (uint16_t)(((uint8_t)mymb.get_a_button()) << 8 | ((uint8_t)mymb.get_b_button()));
+      xQueueSend(queue, &b, 1);
+      delay(20);
+    }
+}
 
 void setup()
 {
   auto cfg = M5.config();
   AtomS3.begin(cfg);
   Serial.begin(115200);
-
   microbit::init();
   microbit::connect(&mymb, MY_PEER);
+
+  queue = xQueueCreate(10, 2);
+  xTaskCreatePinnedToCore(task1, "task1", 4096, NULL, 1, NULL, 1);
 }
 
 void loop()
 {
+  uint16_t b;
   AtomS3.update();
-  Serial.println(std::to_string(mymb.get_a_button()).c_str());
-  Serial.println(std::to_string(mymb.get_b_button()).c_str());
-  delay(100);
+  xQueueReceive(queue, &b, portMAX_DELAY);
+  Serial.println("Hello world!");
+  Serial.println(b);
+  delay(10);
 }
